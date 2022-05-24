656d3ecd034ad77fefd66accdb4fbb8678d891c4
dmitrijpislakov <dpislyakov@bk.ru>
2022-05-24 22:48:35 +0200

    Initial Commit

---
+++ Rainy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rainy
+//
+//  Created by Дмитрий Писляков on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
